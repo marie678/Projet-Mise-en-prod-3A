2a7692c3299dc5067e3e041771d370b86086bd56
Marie <marie.geisp@gmail.com>
2024-12-09 01:58:00 +0100

    first version 3.3 (not functional)

---
+++ App/streamlit/appv3.3.py
@@ -0,0 +1,185 @@
+########################################### app v3.2 #################################################
+# add more filters
+
+import streamlit as st
+import pandas as pd
+from functools import reduce
+from app.config import SAMPLE_RECIPE_PATH, APP_TITLE
+from utils.functions import clean, reformat, split_frame
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+# configuration parameters
+st.set_page_config(layout="wide", page_title ='frigo vide', initial_sidebar_state='collapsed')
+st.title(APP_TITLE)
+
+dev_df = "C:\\Users\\marie\\OneDrive\\Documents\\cours\\ensae\\3A\\infras_et_systemes_logiciels\\df_development"
+df = pd.read_csv(dev_df)
+# df = pd.read_csv(SAMPLE_RECIPE_PATH)
+# df['clean_dir'] = clean(df['directions'])
+
+ingredient_list: set = sorted(set(reformat(df['NER'])))
+recipe_durations: set = sorted(set(df['CookTime_minutes']))
+ratings: set = sorted(set(df['AggregatedRating']))
+
+base: str = r'^{}'
+expr: str = '(?=.*{})'
+
+filter_columns: dict = {
+    'ingredients': 'NER',
+    'recipe_durations': 'CookTime_minutes',
+    'ratings': 'AggregatedRating',
+}
+
+# initialize session_state with recipe elements
+if 'title' not in st.session_state : 
+    st.session_state.title = ''
+if 'ingredients' not in st.session_state :
+    st.session_state.ingredients = ''
+if 'instructions' not in st.session_state:
+    st.session_state.instructions = ''
+if 'link' not in st.session_state:
+    st.session_state.link = ''
+if 'correspondance_rate' not in st.session_state :
+    st.session_state.correspondance_rate = ''
+if 'selected_ingredients' not in st.session_state:
+    st.session_state.selected_ingredients = None
+if 'selected_duration' not in st.session_state:
+    st.session_state.selected_duration = None
+if 'selected_rating' not in st.session_state:
+    st.session_state.selected_rating = None
+
+
+@st.cache_data
+def search_recipes(original_df, filters:list, dict_columns: dict):
+    """
+    dict_columns = corresponding column in a dataset to filter on, for a given filter
+    """
+    filtered_df = original_df.copy()
+    if 'ingredients' in filters.keys():
+        col = dict_columns['ingredients']
+        value = filters['ingredients']
+        filtered_df = filtered_df[filtered_df[col].str.contains(base.format(''.join(expr.format(w) for w in value)))]
+    if 'recipe_durations' in filters.keys():
+        col = dict_columns['recipe_durations']
+        value = filters['recipe_durations']
+        filtered_df = filtered_df[filtered_df[col] == (value)]
+    if 'ratings' in filters.keys():
+        col = dict_columns['ratings']
+        value = filters['ratings']
+        filtered_df = filtered_df[filtered_df[col] >= (value)]
+    # # Compute the correspondance rates
+    # df_search['%'] = df_search['NER'].apply(lambda ing: round((nb_ingredients / len(ast.literal_eval(ing)))*100,1))
+    # df_search = df_search.sort_values('%', ascending=False)
+    total_nr_recipes : int = len(filtered_df)
+    return filtered_df, total_nr_recipes
+
+
+filters = {}
+
+
+with st.form("filter_form", clear_on_submit=True):
+    st.write("Filters")
+    col1, col2, col3, col4 = st.columns(4)
+    
+    ingredients = col1.multiselect("Choose one or more ingredient(s)", ingredient_list, default=st.session_state.selected_ingredients if st.session_state.selected_ingredients else None)
+    st.session_state.selected_ingredients = ingredients  # Update selected ingredients in session state
+    if ingredients:
+        nb_ingredients = len(ingredients)
+        filters['ingredients'] = ingredients
+
+    recipe_time = col2.selectbox("Choose the duration of your recipe", recipe_durations, index=None) #recipe_durations.index(st.session_state.selected_duration) if st.session_state.selected_duration else 
+    st.write(recipe_time)
+    st.session_state.selected_duration = recipe_time  # Update duration in session state
+    if recipe_time: # default to 'Select recipe time'?
+        st.write(recipe_time)
+        st.write(recipe_durations.index(st.session_state.selected_duration))
+        filters['recipe_durations'] = recipe_time
+
+    rating = col3.selectbox("Choose a rating", ratings, index=ratings.index(st.session_state.selected_rating) if st.session_state.selected_rating else None)
+    st.session_state.selected_rating = rating  # Update rating in session state
+    if rating:
+        filters['ratings'] = rating
+    
+    other = col4.selectbox("Choose aother", [1, 2, 3], index=None)
+    # st.session_state.selected_rating = rating  # Store rating in session state
+    if other:
+        filters['other'] = other
+        # st.write(other)
+    #     st.write(type(rating))
+    st.write(filters)
+    submitted = st.form_submit_button("Apply Filters")
+
+if submitted:
+        st.write(filters)
+        df_search, total_nr_recipes = search_recipes(df, filters, filter_columns)
+
+        st.write(f"Il y a **{total_nr_recipes}** recettes correspondant à votre recherche:\n")
+        add_vertical_space(2)
+        recipe_placeholder = st.container()
+        add_vertical_space(2)
+        bottom_menu = st.columns((4,1,1))
+        with bottom_menu[1]:
+            batch_size = st.selectbox('Nombre de recettes par page', options=[25,50,100])
+            total_pages = int(len(df_search)/batch_size) if len(df_search)>batch_size else 1
+        with bottom_menu[2]:
+            current_page = st.number_input('Page', min_value=1, max_value=total_pages, step=1, key='page_input')
+        with bottom_menu[0]:
+            st.markdown(f"Page **{current_page}** de **{total_pages}**")
+
+        # Split the DataFrame to get the current page data
+        start_idx = (current_page - 1) * batch_size
+        end_idx = start_idx + batch_size
+        page = df_search.iloc[start_idx:end_idx]
+
+        # pages = split_frame(df_search, batch_size+1)
+        # page = (pages[current_page - 1])
+
+        # faire le lien avec fichier CSS, pas encore actif
+        st.markdown(
+        """
+        <style>
+        button {
+            background: none!important;
+            border: none;
+            padding: 0!important;
+            color: black !important;
+            text-decoration: none;
+            cursor: pointer;
+            border: none !important;
+        }
+        button:hover {
+            text-decoration: none;
+            color: black !important;
+        }
+        button:focus {
+            outline: none !important;
+            box-shadow: none !important;
+            color: black !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+        for i in range(len(page)) :
+            if recipe_placeholder.button(page.iloc[i]['title'], key=f"recipe_button_{i}"):
+                st.session_state.title = page.iloc[i]['title']
+                st.session_state.ingredients = page.iloc[i]['ingredients']
+                st.session_state.instructions = page.iloc[i]['directions']
+                st.session_state.link = page.iloc[i]['link']
+                st.session_state.correspondance_rate = page.iloc[i]['%']
+                with st.spinner() :
+                    st.switch_page("./pages/Recettes.py")
+
+
+# reset_button = st.button('Réinitialiser')
+# if reset_button :
+#     st.rerun()
+#     st.session_state.search_triggered = False
+#     st.session_state.selected_ingredients = []
+#     st.session_state.batch_size = 25
+#     st.session_state.current_page = 1
+
+#     ingredients = []  # Reset selected ingredients
+#     nb_ingredients = 0
+#     page = None  # Clear the current page
+#     st.empty()  # Clear all dynamic elements in the app
