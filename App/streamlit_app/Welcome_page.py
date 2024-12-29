8d5529937533e437e7327451af9d5d899a5a0f3d
Bipauli <pauline.bian7@gmail.com>
2024-12-30 00:57:01 +0100

    recover title Fridge&Cook

--- App/streamlit_app/Welcome_page.py
+++ App/streamlit_app/Welcome_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 
+st.set_page_config(layout="wide", page_title ='FRIDGE & COOK', initial_sidebar_state='collapsed')
 def display_html_in_streamlit(html_file_path):
     """Displays HTML content from a file in a Streamlit app.
 
