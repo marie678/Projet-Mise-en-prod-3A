03cfb6bb5da284aeb973a3e93ff14b6df061a07e
Marie <marie.geisp@gmail.com>
2024-12-30 20:36:33 +0100

    small modifs

--- dev/App/streamlit_app/Welcome_page.py
+++ dev/App/streamlit_app/Welcome_page.py
@@ -11,25 +11,17 @@ def display_html_in_streamlit(html_file_path):
     try:
         with open(html_file_path, "r", encoding="utf-8") as f:
             html_content = f.read()
-        st.components.v1.html(html_content, height=1500, width=1400, scrolling=True)
+        st.components.v1.html(html_content, height=1500, width=1080, scrolling=True)
     except FileNotFoundError:
         st.error(f"Error: HTML file not found at {html_file_path}")
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
 # Example usage (adjust the path as needed):
-html_file_path = "Welcome page.html"  # Or a full path like "C:/path/to/your/file.html"
+html_file_path = "Welcome page.html"
 
 # Check if the file exists before attempting to display it
 if os.path.exists(html_file_path):
     display_html_in_streamlit(html_file_path)
 else:
     st.error(f"The HTML file '{html_file_path}' does not exist. Please check the path.")
-
-print(html_file_path)
-
-# Display images using st.image
-#image1 = st.image("images/go_to_Recipe_Finder_page.png")
-#image2 = st.image("images/hit_find_recipe_button.png")
-#image3 = st.image("images/results_avocado_search.png")
-#image4 = st.image("images/printable_recipe.png")
